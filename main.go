43a9b8af0d40761685ddd7d95e1cd3175514d2fd
Louistam888 <louistam888@gmail.com>
2025-09-13 12:47:22 -0400

    program complete

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode"
+)
 
 func main() {
-	fmt.Println("first commit")
+input := "Madam, I'm Adam"
+
+normalized := ""
+//remove non letter characters and convert to lowercase
+for _, r := range input {
+	if unicode.IsLetter(r) {
+		normalized += string(unicode.ToLower(r))
+	}
+}
+
+
+var isPalindrome bool = true
+
+length := len(normalized)
+
+for i:=0; i<length/2; i++ {
+	if normalized[i] != normalized[length-1-i] {
+		isPalindrome = false
+	}
+}
+
+if isPalindrome {
+	fmt.Println("It is a palinedrome")
+} else {
+	fmt.Println("It is not a palindrome")
+}
+
+
 }
